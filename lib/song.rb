61d11df71f55e030c917ed619886835040050e00
Emily Welsch <emwelsch@gmail.com>
2018-05-25 16:35:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ class Song
     end
   end
 
-  def self.find_or_create_by_name(name)
+  def self.find_or_create_by_name(name = nil)
     self.find_by_name(name) || self.create_by_name(name)
   end
 
