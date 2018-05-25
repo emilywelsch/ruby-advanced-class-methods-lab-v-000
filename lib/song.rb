9832c0f1cd9cf9a95b8bd1db6b768e80fa81b173
Emily Welsch <emwelsch@gmail.com>
2018-05-25 15:37:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,39 @@ class Song
     self.class.all << self
   end
 
+  def initialize(song)
+      @song = song
+      @@all << self
+    end
+
+  def self.create
+    self.new
+  end
+
+  def self.new_by_name
+  end
+
+  def self.create_by_name
+  end
+
+  def self.find_by_name
+  end
+
+  def self.find_or_create_by_name
+  end
+
+  def self.alphabetical # why does it want #sort_by when #sort will do?
+    @@all.sort_by { |song| song.sort }
+  end
+
+  def self.new_from_filename
+  end
+
+  def self.create_from_filename
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
 end
